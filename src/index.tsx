2af25fd6c996eb5ef48a14041b5b9bf5764859ce
Alf Eaton <eaton.alf@gmail.com>
2019-07-22 21:06:16 +0100

    Only add the SerciceWorker in production

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,17 @@ import { App } from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then(registration => {
-        console.log('SW registered: ', registration)
-      })
-      .catch(registrationError => {
-        console.log('SW registration failed: ', registrationError)
-      })
-  })
+if (process.env.NODE_ENV === 'production') {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then(registration => {
+          console.log('SW registered: ', registration)
+        })
+        .catch(registrationError => {
+          console.log('SW registration failed: ', registrationError)
+        })
+    })
+  }
 }
